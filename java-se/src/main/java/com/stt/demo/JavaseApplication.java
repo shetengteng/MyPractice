994c9fd11293c554b8ed332b8c16bc6fff45b763
shetengteng <382143256@qq.com>
2018-09-21 10:56:52 +0800

    first commit

---
+++ java-se/src/main/java/com/stt/demo/JavaseApplication.java
@@ -0,0 +1,12 @@
+package com.stt.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaseApplication.class, args);
+	}
+}
